99d45471865e43083492be9519a358b0c1602725
ChildLee <471227142@qq.com>
2018-07-31 20:23:08 +0800

    添加配置

---
+++ app/model/index.js
@@ -0,0 +1,4 @@
+const Sequelize = require('sequelize')
+const config = require('../../config/config.default')
+
+const sequelize = new Sequelize(config.sequelize)
---
+++ config/config.default.js
@@ -0,0 +1,28 @@
+exports.sequelize = {
+  dialect: 'mysql',
+  host: 'localhost',
+  port: 3306,
+  username: 'root',
+  password: 'sa',
+  database: 'rbac',
+
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  },
+
+  timezone: '+08:00',
+  operatorsAliases: false,
+  define: {
+    // 禁止修改表名
+    freezeTableName: true,
+    // 下划线命名
+    underscored: true
+  },
+
+  logging: (sql) => {
+    console.log(sql)
+  }
+}
